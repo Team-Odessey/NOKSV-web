9361e910b47a75444911e701f1e4dbd970358247
Cartridge0923 <pjmin0923@gmail.com>
2025-07-16 06:07:25 +0900

    Update Router.tsx

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -7,27 +7,33 @@ import ProfilePage from '@src/pages/ProfilePage'
 import GuildPage from '@src/pages/GuildPage'
 import GuildDetail from '@src/pages/GuildPage/GuildDetail'
 import NotFoundPage from '@src/pages/NotFoundPage'
-import Login from '@src/components/auth/Login'
+import Auth from '@src/pages/Auth'
 import RankingPage from '@src/pages/RankingPage'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient();
 
 const Router = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout/>}>
-          <Route index element={<MainPage/>}/>
-          <Route path='login' element={<Login/>}/>
-          <Route path='/notification' element={<NotificationPage/>}/>
-          <Route path='/notification/:id' element={<NotificationDetail/>}/>
-          <Route path='/profile' element={<ProfilePage/>}/>
-          <Route path='/profile/:id' element={<ProfilePage/>}/>
-          <Route path='/guild' element={<GuildPage/>} />
-          <Route path='/guild/:name' element={<GuildDetail/>} />
-          <Route path='/ranking' element={<RankingPage/>}/>
-          <Route path='/*' element={<NotFoundPage/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout/>}>
+            <Route index element={<MainPage/>}/>
+            <Route path='/login' element={<Auth type="LOGIN"/>}/>
+            <Route path='/signup' element={<Auth type="SIGNUP"/>}/>
+            <Route path='/notification' element={<NotificationPage/>}/>
+            <Route path='/notification/:id' element={<NotificationDetail/>}/>
+            <Route path='/profile' element={<ProfilePage/>}/>
+            <Route path='/profile/:id' element={<ProfilePage/>}/>
+            <Route path='/guild' element={<GuildPage/>} />
+            <Route path='/guild/:name' element={<GuildDetail/>} />
+            <Route path='/ranking' element={<RankingPage/>}/>
+            <Route path='/*' element={<NotFoundPage/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
