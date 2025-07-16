ac6c2e70bb20829b1c8a520111a998fcdc4b7bf9
Cartridge0923 <pjmin0923@gmail.com>
2025-07-16 10:26:12 +0900

    feat: auth alert

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -8,9 +8,10 @@ import axios, { AxiosResponse } from 'axios';
 import CONFIG from '@src/config/config.json'
 import { AuthSignUpRes } from '@src/types/auth/auth.type';
 import token from '@src/libs/token/token';
+import { Link } from 'react-router-dom';
 
 const Auth = ({type}: {type: "LOGIN" | "SIGNUP"}) => {
-  const { inputData: idData, handleChangeInput: handleChangeId } = useInputBar()
+  const { inputData: idData, handleChangeInput: handleChangeId, resetInput: resetId } = useInputBar()
   const { inputData: passwordData, handleChangeInput: handleChangePassword, resetInput: resetPassword } = useInputBar()
 
   const nav = useNavigate();
@@ -21,7 +22,7 @@ const Auth = ({type}: {type: "LOGIN" | "SIGNUP"}) => {
         token.setToken('access-token', item.data.accessToken)
         token.setToken('refresh-token', item.data.refreshToken)
         nav('/')
-      })
+      }).catch(() => alert('로그인 실패! 아이디, 비밀번호를 확인해주세요.'))
     } catch {
       console.log('err')
       alert('로그인 실패! 아이디, 비밀번호를 확인해주세요.')
@@ -34,7 +35,12 @@ const Auth = ({type}: {type: "LOGIN" | "SIGNUP"}) => {
       axios.post(`${CONFIG.server}/api/auth/signup`, param).then((item: AxiosResponse<AuthSignUpRes>) => {
         token.setToken('access-token', item.data.accessToken)
         token.setToken('refresh-token', item.data.refreshToken)
+        alert('녹서버에 오신 것을 환영합니다!')
         nav('/')
+      }).catch(() => {
+        alert('회원가입 실패! 올바른 인게임 이름을 입력해주세요.')
+        resetId()
+        resetPassword()
       })
     } catch {
       console.log('err')
@@ -53,6 +59,9 @@ const Auth = ({type}: {type: "LOGIN" | "SIGNUP"}) => {
         width='280px'
         onClickFn={() => type === "LOGIN" ? verifyLogin({username: idData, password: passwordData}) : verifySignUp({username: idData, password: passwordData})}
       />
+      <S.LoginToSignUpLink to={type === "LOGIN" ? "/signup" : "/login"}>
+        {type === "LOGIN" ? "회원가입" : "로그인"}하러 가기
+      </S.LoginToSignUpLink>
     </S.LoginContainer>
   )
 }
--- src/pages/Auth/style.ts
+++ src/pages/Auth/style.ts
@@ -1,6 +1,7 @@
 import { nokPalette } from "@src/constants/color/color.constants";
 import { nokTypograpy } from "@src/constants/font/font.constants";
 import { MainContentsBox } from "@src/styles/globalStyles";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const LoginContainer = styled(MainContentsBox)`
@@ -10,4 +11,9 @@ export const LoginContainer = styled(MainContentsBox)`
   color: ${nokPalette.labelNormal};
   ${nokTypograpy.VITRO.Extra2};
   min-height: 760px;
+`
+
+export const LoginToSignUpLink = styled(Link)`
+  color: ${nokPalette.labelAlternative};
+  ${nokTypograpy.Pretendard.Caption1.Medium}
 `
